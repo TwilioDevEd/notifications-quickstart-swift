828034058d403f3b31b4a3a8eccea88946bc7041
sraval <sraval@twilio.com>
2016-02-24 16:26:38 -0800

    first

--- notifications/ViewController.swift
+++ notifications/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+  var serverURL : String = "http://9e6cb053.ngrok.io/register"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  @IBOutlet var registerButton: UIButton!
+  @IBOutlet var identityField: UITextField!
+    
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func didTapRegister(sender: UIButton) {
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    let deviceToken : String! = appDelegate.devToken
+    let identity : String! = self.identityField.text
+    registerDevice(identity, deviceToken: deviceToken)
+  }
+    
+  func registerDevice(identity: String, deviceToken: String) {
+    //Init request
+    Alamofire.request(.POST, serverURL, parameters: ["identity": identity,
+                                                    "endpoint" : identity+deviceToken,
+                                                 "BindingType" : "apn",
+                                                     "Address" : deviceToken])
+            .responseJSON { response in
+              let result = NSString(data: response.data!, encoding: NSUTF8StringEncoding)
+              if result != "Success" {
+                print("Error: registration not successful")
+              }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
