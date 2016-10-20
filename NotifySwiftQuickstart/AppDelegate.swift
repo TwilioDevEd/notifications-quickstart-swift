ea625264450899f5d4c0c94b77c5c8699af8cf46
Jeffrey Linwood <jlinwood@gmail.com>
2016-10-20 11:57:31 -0500

    Update push notification registration to use UserNotifications framework

--- NotifySwiftQuickstart/AppDelegate.swift
+++ NotifySwiftQuickstart/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import UserNotifications
+
 
 @UIApplicationMain
 
@@ -17,9 +19,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
-    let settings = UIUserNotificationSettings(types: .alert, categories: nil)
-    UIApplication.shared.registerUserNotificationSettings(settings)
-    UIApplication.shared.registerForRemoteNotifications()
+    if #available(iOS 10, *) {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert], completionHandler: { (granted, error) in
+            UIApplication.shared.registerForRemoteNotifications()
+        })
+    } else {
+        let settings = UIUserNotificationSettings(types: .alert, categories: nil)
+        UIApplication.shared.registerUserNotificationSettings(settings)
+        UIApplication.shared.registerForRemoteNotifications()
+    }
+
     return true
   }
   
